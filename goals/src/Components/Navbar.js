fc597214ddaf4924d75c8d3c84c748a2ce50fa73
asenso-clarissa <asenso.clarissa@yahoo.com>
2020-11-30 11:51:55 +0100

    Added Navbar

---
+++ goals/src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <footer>
+                <ul>
+                <li><img src="" alt="Homeicon" /> Home</li>
+                <li>Workout</li>
+                <li>Recipies</li>
+                <li>Profile</li>
+                </ul>
+        </footer>
+        </div>
+    )
+}
+
+export default Navbar;
