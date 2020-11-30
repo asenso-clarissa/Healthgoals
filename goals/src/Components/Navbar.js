ed59820513ea89e3a6f4e5c1333cbe0b57718232
asenso-clarissa <asenso.clarissa@yahoo.com>
2020-11-30 12:06:27 +0100

    navigation icons added

--- goals/src/Components/Navbar.js
+++ goals/src/Components/Navbar.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
+import homeIcon from '../img/home.png'
 
 function Navbar() {
-    return (
-        <div>
-            <footer>
-                <ul>
-                <li><img src="" alt="Homeicon" /> Home</li>
-                <li>Workout</li>
-                <li>Recipies</li>
-                <li>Profile</li>
-                </ul>
-        </footer>
-        </div>
-    )
+  return (
+    <div>
+      <footer>
+        <ul>
+          <li>
+            <img src={homeIcon} alt="Homeicon" style={{width: '20px'}} /> Home
+          </li>
+          <li>Workout</li>
+          <li>Recipies</li>
+          <li>Profile</li>
+        </ul>
+      </footer>
+    </div>
+  );
 }
 
 export default Navbar;
